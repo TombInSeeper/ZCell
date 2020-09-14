c0ffa91255dc91063ec3ebe04c20fce43ed826e6
wuyue <wynebula@qq.com>
2020-09-14 20:42:59 +0800

    update

---
+++ chrono.h
@@ -0,0 +1,16 @@
+
+#ifndef CHRONO_H
+#define CHRONO_H
+
+#include <sys/time.h>
+#include <stdint.h>
+
+static  inline uint64_t now() {
+    struct timeval t;
+    gettimeofday(&t,0);
+    return t.tv_sec * 1000000 + t.tv_usec;
+}
+
+
+
+#endif
