7a2a76d561977efd85d3679a92644140ab4aa672
wuyue <wynebula@qq.com>
2020-09-14 19:53:49 +0800

    update

--- test_objstore.c
+++ test_objstore.c
@@ -10,6 +10,10 @@
 
 static int g_store = CHUNKSTORE;
 static const char* g_nvme_dev[] = { "Nvme0n1" , NULL, NULL };
+static int g_nr_ops = 10000;
+static int g_nr_cpl = 0;
+static uint64_t g_start_tsc;
+static uint64_t g_end_tsc;
 
 const objstore_impl_t *os;
 void *session;
@@ -100,21 +104,65 @@ const message_t fake_read_request_msg = {
 };
 
 
+void* _alloc_op() {
+    void *p = calloc(1, sizeof(message_t) + 16);
+    return p;
+}
+
+void _free_op(void *p) {
+    free(p);
+}
 
 void _sys_fini()
 {
+    os->unmount();
+
+
+    spdk_free(rbuf);
+    spdk_free(wbuf);
+
     spdk_app_stop(0);
 }
 
 
-void _do_uint_test()
-{
-    os->unmount();
+void _write_complete(void *ctx, int sts) {
+    g_nr_cpl++;
+    if(g_nr_cpl >= g_nr_ops) {
+        free(ctx);
+        _sys_fini();
+    } else {
+       os->obj_async_op_call(ctx, _write_complete);
+    }
 }
 
-void _sys_init(void *arg)
-{
+void _do_uint_test() {
+    int dp = 64;
+    void *op = _alloc_op();
+    memcpy(op, &fake_write_request_msg,sizeof(fake_read_request_msg));
+
+    message_t *m = op;
+    m->meta_buffer = meta_buffer;
+
+    op_write_t *_op_args = (void*)m->meta_buffer;
+    _op_args->len = 0x1000;
+    _op_args->ofst = 0x10000;
+    _op_args->oid = 0x0;
+    _op_args->flags = 0x0;
+
+    m->data_buffer = wbuf;
+
+    while (--dp) {
+       os->obj_async_op_call(op, _write_complete);
+    }
+}
+
+void _sys_init(void *arg) {
     (void)arg;
+
+    rbuf = spdk_dma_zmalloc(0x1000 * 1024, 0x1000, NULL);
+    wbuf = spdk_dma_zmalloc(0x1000 * 1024, 0x1000, NULL);
+
+
     os = ostore_get_impl(g_store);
     int rc = os->mkfs(g_nvme_dev,0);
     assert(rc == SUCCESS);
