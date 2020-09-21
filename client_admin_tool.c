741e07ad409a0501f1ef528b7296a1ece476431c
wuyue <wynebula@qq.com>
2020-09-21 20:54:30 +0800

    update

--- client_admin_tool.c
+++ client_admin_tool.c
@@ -15,6 +15,7 @@ typedef struct admin_context_t {
     
     //Liboss
     io_channel *ioch;
+
 } admin_context_t;
 
 typedef int (*handler_func) (admin_context_t *actx);
@@ -32,15 +33,15 @@ typedef struct command_handler_t {
 void print_usage_and_exit() {
     const char *_usage = "client_admin [ip] [port] [sub_command(stat/put/get/delete/perf)] [sub_args] \
     [NOTICE]:put 传入的字符串长度不能长于 4095 \n\
-    [NOTICE]:perf 创建 10,000 个 4M 对象，生成 100,000 次 4K rand w/r 操作, 输出 IOPS 和 lat \n\
+    [NOTICE]:perf 创建 10,000 个 4M 对象，生成 (qd * rw_loop) 次 4K rand w/r 操作, 输出 lat \n\
     put [object_id] [string]\n\
     get [object_id] \n\
     delete [object_id] \n\
-    perf [seq/rand] [qd] \n\
+    perf  [qd] [rw_loop] \n\
     e.x.(put)  ./client_admin 10.0.12.10 6677 put 0 \"XYZABCDE\" \n\
     e.x.(get)  ./client_admin 10.0.12.10 6677 get 0 \n\
     e.x.(delete)  ./client_admin 10.0.12.10 6677 delete 0 \n\
-    e.x.(perf)  ./client_admin 10.0.12.10 6677 perf  \n\
+    e.x.(perf)  ./client_admin 10.0.12.10 6677 perf  32 1000\n\
     \
     ";
     printf(_usage);
@@ -61,8 +62,6 @@ static int _do_create_or_delete_test_objects(admin_context_t *ac , int create) {
     int *opds_cpl = malloc(256 * sizeof(int));
     const int cqd = 100;
     log_info("Creating %d objects..\n" , n_objs);
-    
-    
     uint64_t cre_st = now();
     for (i = 0 ; i < n_objs / (cqd) ; ++i) {
         int j;
@@ -119,9 +118,153 @@ static int _do_create_or_delete_test_objects(admin_context_t *ac , int create) {
     return 0;
 }
 
-int _do_perf(admin_context_t *ac, int qd) {
+static int _do_write_and_read_test_objects(admin_context_t *ac, int loop_, int qd ) {
+    int i;
+    int loop = loop_;
+    int *opds = malloc (256 * sizeof(int));
+    int *opds_cpl = malloc(256 * sizeof(int));
+    const int cqd = qd;
+    log_info("Prepare to issue random %d write requests \n.." , loop * qd); 
+    
+    _perf_io_t *ios = malloc(sizeof(_perf_io_t) * (loop * cqd));
+
+    void *wbuff; 
+    io_buffer_alloc(&wbuff, 0x1000);    
+    uint64_t cre_st = now();    
+    for (i = 0 ; i < loop ; ++i) {
+        int j;
+        for (j = 0 ; j < cqd ; ++j) {
+            uint32_t oid = rand() % 10000;
+            uint32_t ofst = (rand() % 1024) * 0x1000;
+            uint32_t len = 0x1000;
+            opds[j] = io_write(ac->ioch , oid , wbuff , ofst , len);
+
+            ios[ i * cqd + j ].oid  = oid;
+            ios[ i * cqd + j ].ofst = ofst ;
+            ios[ i * cqd + j ].len = len;
+
+            // log_debug("opd=%d\n",opds[j]);
+            if(opds[j] < 0) {
+                log_err("Unexpected opd:%d\n",opds[j]);    
+                exit(1);
+            }
+            log_debug("opds[%d]=%d\n", j, opds[j]);
+        }
+
+        // exit(1);
+        int rc = io_submit_to_channel(ac->ioch, opds , cqd) ;
+        if(rc < 0) {
+            log_err("Submit error\n");
+            exit(1);
+        }        
+        
+        rc = io_poll_channel(ac->ioch,opds_cpl,cqd,cqd);
+        if(rc < 0) {
+            log_err("Poll error\n");
+            exit(1);
+        }
+        
+        for ( j = 0; j < cqd ; ++j) {
+            int status;
+            op_claim_result(ac->ioch, opds_cpl[j], &status, NULL, NULL, NULL);
+            if(status) {
+                log_err("Unexpected status:%d, op_id=%d\n",status,opds_cpl[j]);    
+                exit(1);
+            }
+            op_destory(ac->ioch, opds_cpl[j]);
+        }
+    }    
+    uint64_t cre_ed = now();
+    io_buffer_free(wbuff);
+    double cre_dur = cre_ed - cre_st;
+    double cre_lat = cre_dur / (loop * cqd);        
+    log_info("%d 4K write time:%lf us , avg_lat=%lf us \n",  (loop * cqd), cre_dur, cre_lat);
+
+
+
+    cre_st = now();    
+    for (i = 0 ; i < loop ; ++i) {
+        int j;
+        for (j = 0 ; j < cqd ; ++j) {
+            uint32_t oid = ios[ i * cqd + j ].oid;
+            uint32_t ofst = ios[ i * cqd + j ].ofst;
+            uint32_t len = ios[ i * cqd + j ].len;
+            opds[j] = io_read(ac->ioch , oid , ofst , len);
+
+            // log_debug("opd=%d\n",opds[j]);
+            if(opds[j] < 0) {
+                log_err("Unexpected opd:%d\n",opds[j]);    
+                exit(1);
+            }
+            log_debug("opds[%d]=%d\n", j, opds[j]);
+        }
+
+        // exit(1);
+        int rc = io_submit_to_channel(ac->ioch, opds , cqd) ;
+        if(rc < 0) {
+            log_err("Submit error\n");
+            exit(1);
+        }        
+        
+        rc = io_poll_channel(ac->ioch,opds_cpl,cqd,cqd);
+        if(rc < 0) {
+            log_err("Poll error\n");
+            exit(1);
+        }
+        
+        for ( j = 0; j < cqd ; ++j) {
+            int status;
+            void *rbuf;
+            uint32_t rlen;
+            op_claim_result(ac->ioch, opds_cpl[j], &status, NULL, &rbuf, &rlen);
+            log_debug("Without verifying\n");
+            if(status) {
+                log_err("Unexpected status:%d, op_id=%d\n",status,opds_cpl[j]);    
+                exit(1);
+            }
+            io_buffer_free(rbuf);
+            op_destory(ac->ioch, opds_cpl[j]);
+        }
+    }    
+    cre_ed = now();
+    cre_dur = cre_ed - cre_st;
+    cre_lat = cre_dur / (loop * cqd);        
+    log_info("%d 4K Read time:%lf us , avg_lat=%lf us \n",  (loop * cqd), cre_dur, cre_lat);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    free(ios);
+    free(opds);
+    free(opds_cpl);
+    return 0;
+}
+
+int _do_perf(admin_context_t *ac, int rw_loop, int qd) {
     log_info("Perf start...\n");
     _do_create_or_delete_test_objects(ac , 1);
+    
+    _do_write_and_read_test_objects(ac, rw_loop, qd);
+    
     _do_create_or_delete_test_objects(ac , 0);
     log_info("Perf done...\n");
     return 0;
@@ -299,7 +442,8 @@ int _run(admin_context_t *ac, int argc , char **argv) {
         return 0;
     } else if (!strcmp(cmd, "perf")){
         int qd = atoi(argv[4]);
-        if(_do_perf(ac ,qd)) {
+        int rw_loop = atoi(argv[5]);
+        if(_do_perf(ac ,qd , rw_loop)) {
             log_err("Perf failed\n");
             return -1;
         }
