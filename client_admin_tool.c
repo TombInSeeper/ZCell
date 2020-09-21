1dff39d821a5b20ac08c93defce1549029f093a2
wuyue <wynebula@qq.com>
2020-09-21 16:11:29 +0800

    update

--- client_admin_tool.c
+++ client_admin_tool.c
@@ -8,6 +8,11 @@
 #include <string.h>
 #include <sys/stat.h>
 
+
+const char dummpy_write_str[] = "Yue xia hong yan zhuang , que wei zuo jia mang\n";
+
+
+
 typedef struct admin_context_t {
     const char *ip;
     int port;
@@ -25,31 +30,22 @@ typedef struct command_handler_t {
 } command_handler_t;
 
 
-const char *sub_cmds = {
-    ""
-}; 
 
 
 void print_usage_and_exit() {
     const char *_usage = "client_admin [ip] [port] [sub_command(stat/put/get/delete)] [sub_args] \
-    [NOTICE]:PUT/GET 传入文件必须是 4K(4096bytes) 大小对齐的\n\
-    put [object_id] [content file]\n\
-    get [object_id] [result file]\n\
+    [NOTICE]:put 传入的字符串长度不能长于 4095 \n\
+    put [object_id] [string]\n\
+    get [object_id] \n\
     delete [object_id] \n\
-    e.x.(put)  ./client_admin 10.0.12.10 6677 put 0 FOO.txt \n\
-    e.x.(get)  ./client_admin 10.0.12.10 6677 get 0 FOO.txt \n\
+    e.x.(put)  ./client_admin 10.0.12.10 6677 put 0 \"XYZABCDE\" \n\
+    e.x.(get)  ./client_admin 10.0.12.10 6677 get 0 \n\
     e.x.(delete)  ./client_admin 10.0.12.10 6677 delete 0 \n\
     \
     ";
     printf(_usage);
     exit(1);
 } 
-
-
-
-
-
-
 
 int _sync_with_op(admin_context_t *ac , int opd) {
     int rc = io_submit_to_channel(ac->ioch, &opd, 1);
@@ -95,30 +91,15 @@ int _do_delete(admin_context_t *ac , uint32_t oid) {
     return ac->errcode;
 }
 
-int _do_write(admin_context_t *ac, uint32_t oid, const char *file) {
-    struct stat _fstat;
-    if (stat(file, &_fstat)) {
-        log_err("Get attr of %s falied, errstr:%s \n", file, strerror(errno));
-        return -1;
-    }
-    if (_fstat.st_size % 4096 != 0) {
-        log_err("File size isn't aligned with 4K \n");
+int _do_write(admin_context_t *ac, uint32_t oid, const char *str) {
+    if (strlen(str) >= 4096) {
+        log_err("String is too long\n");
         return -1;
     }
     // uint32_t fsz = _fstat.st_size;
     uint32_t fsz = 0x1000;
     void *iobuf;
     io_buffer_alloc(&iobuf, fsz);    
-    do {
-        FILE *fp = fopen(file, "r");
-        size_t read_bytes = fread(iobuf, fsz , 1, fp);
-        if(read_bytes != fsz) {
-            log_err(" File IO error\n");
-            fclose(fp);
-            return -1;        
-        }
-        fclose(fp);
-    } while(0);
     int opd = io_write(ac->ioch, oid, iobuf, 0, fsz);
     log_debug("opd=%d, prepare OK\n", opd);
     
@@ -137,7 +118,7 @@ int _do_write(admin_context_t *ac, uint32_t oid, const char *file) {
     return status;
 }
 
-int _do_get(admin_context_t *ac, uint32_t oid, const char *file) {
+int _do_get(admin_context_t *ac, uint32_t oid) {
     int opd = io_read(ac->ioch, oid , 0 , 0x1000);
     _sync_with_op(ac , opd);
     //
@@ -150,11 +131,9 @@ int _do_get(admin_context_t *ac, uint32_t oid, const char *file) {
     data_buffer != NULL ? ({(void)0;}):({log_warn("Read op response data_buffer is NULL\n");});
     op_destory(ac->ioch, cpl);
 
-    FILE *fp = fopen(file, "w");
-    do {
-        fwrite(data_buffer, 0x1000, 1, fp);
-    } while(0);
-    fclose(fp);
+    char *_str = data_buffer;
+    _str[4095] = '\0';
+    log_info("\n%s\n", _str);
 
     io_buffer_free(data_buffer);
     return status;
@@ -221,8 +200,7 @@ int _run(admin_context_t *ac, int argc , char **argv) {
         return 0;
     } else if (!strcmp(cmd, "get")){
         uint32_t oid = atoi(argv[4]);
-        const char *file = argv[5];
-        if(_do_get(ac, oid , file)) {
+        if(_do_get(ac, oid)) {
             log_err("Read object %u failed\n" ,oid);
             return -1;
         }
