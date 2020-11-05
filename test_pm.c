2d46cbade03a18c16206f284bedb886c810fee70
wuyue <wynebula@qq.com>
2020-11-05 11:23:13 +0800

    update

--- test_pm.c
+++ test_pm.c
@@ -63,15 +63,15 @@ int main(int argc , char **argv) {
     stupid_allocator_constructor(al ,1024 * 1024);
     struct zstore_extent_t ze[64];
     uint64_t en;
-    stupid_alloc_space(al, 1 ,ze, &en);
+    stupid_alloc_space(al, 2 ,ze, &en);
     // assert(en == 1);
     // assert(ze[0].lba_ == 0 && ze[0].len_ == 1);
     alloc_result_str(ze,en);
 
-    stupid_alloc_space(al, 63 ,ze, &en);
-    // assert(en == 1);
-    // assert(ze[0].lba_ == 1 && ze[0].len_ == 63);
-    alloc_result_str(ze,en);
+    // stupid_alloc_space(al, 63 ,ze, &en);
+    // // assert(en == 1);
+    // // assert(ze[0].lba_ == 1 && ze[0].len_ == 63);
+    // alloc_result_str(ze,en);
 
     stupid_allocator_destructor(al);
 
