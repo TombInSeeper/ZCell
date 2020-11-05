0227a756e916a3ce95ac9051ddb7d19e7aa1ae29
wuyue <wynebula@qq.com>
2020-11-05 20:28:12 +0800

    update

--- zstore.c
+++ zstore.c
@@ -201,14 +201,18 @@ extern int zstore_mkfs(const char *dev_list[], int flags) {
 
     uint64_t nblks = spdk_bdev_get_num_blocks(zstore->nvme_bdev_);
     uint64_t nblks_ = FLOOR_ALIGN(nblks, 512);
-    log_info("Block number = %lu , floor_align 512 to %lu", nblks , nblks_);
-    assert (nblks == nblks_);
-    
+    log_info("SSD Block number = %lu , floor_align 512 to %lu\n", nblks , nblks_);
+    // assert (nblks == nblks_);
+    zsb->ssd_nr_pages = nblks;
 
     uint64_t npm_blks_; 
     rc = zstore_pm_file_open(zstore, dev_list[1], &npm_blks_);
-    npm_blks_ = npm_blks_ >> 12;
     assert(rc == 0);
+
+    npm_blks_ = npm_blks_ >> 12;
+    log_info("PM Block number = %lu , floor_align 512 to %lu\n", npm_blks_ , FLOOR_ALIGN(npm_blks_, 512) );
+
+    zsb->pm_nr_pages = npm_blks_;
 
     uint64_t onode_rsv = 1ULL << 20;
     zsb->pm_ssd_bitmap_ofst = 1ULL << 20;
