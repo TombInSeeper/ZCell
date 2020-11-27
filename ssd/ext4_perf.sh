fe68390bbcacaf028ba7c402d25bd8aa6403a33a
wuyue <wynebula@qq.com>
2020-11-27 14:58:34 +0800

    update

--- ssd/ext4_perf.sh
+++ ssd/ext4_perf.sh
@@ -10,7 +10,7 @@ dir=/mnt/ext4
 fio --ioengine=libaio --direct=1 --thread --norandommap \
 --nrfiles=${nrfiles}  --directory=${dir} --filename_format=test.'$'jobname.'$'filenum\
 --size=${totalsize} \
-# --name=ext4_init_seq \
+--name=ext4_init_seq \
 --output=/run/perf/ext4/init_seq.log --rw=write --bs=128k \
 --numjobs=4 \
 --log_avg_msec=500\
