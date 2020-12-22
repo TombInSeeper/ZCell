39e24576a19460e8367743587215e410924da891
wuyue <wynebula@qq.com>
2020-12-22 16:05:01 +0800

    up

--- util/common.h
+++ util/common.h
@@ -63,10 +63,10 @@ typedef uint64_t _le64;
 #define FLOOR_ALIGN(v , align) (((v)) & (~((align)-1)) ) 
 
 #define SWAP(x,y) do \
-    {   unsigned char swap_temp[sizeof(x) == sizeof(y) ? (signed)sizeof(x) : -1]; \
-        memcpy(swap_temp,&y,sizeof(x)); \
-        memcpy(&y,&x,       sizeof(x)); \
-        memcpy(&x,swap_temp,sizeof(x)); \
-    } while(0)
+{   unsigned char swap_temp[sizeof(x) == sizeof(y) ? (signed)sizeof(x) : -1]; \
+    memcpy(swap_temp,&y,sizeof(x)); \
+    memcpy(&y,&x,       sizeof(x)); \
+    memcpy(&x,swap_temp,sizeof(x)); \
+} while(0)
 
 #endif
